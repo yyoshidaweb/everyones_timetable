85a267e24abb049d2b5ce7ae5e94f5c05b99ea6c
yyoshidaweb <yoshidayumajob@gmail.com>
2025-01-12 17:33:15 +0900

    fix: Chromeの検証モード利用時に発生するエラーに対処した

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   include Authentication
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  # 開発中はコメントアウトしておく（最新ブラウザのみを許可する設定）
+  # allow_browser versions: :modern
 
   # ロケールを変更する
   around_action :switch_locale
